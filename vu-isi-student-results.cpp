7e5aa0b685fc53335c2f0c867c92e5492187ae8d
Vladas Zvonkus <vladas.zvonkus@gmail.com>
2020-03-10 22:23:14 +0200

    .txt in .gitignore

--- vu-isi-student-results.cpp
+++ vu-isi-student-results.cpp
@@ -66,7 +66,7 @@ void runTests(RandomGenerator* generator)
 	const string testPath = "test.txt";
 	const double gradeBound = 5.0;
 	const int grade_count = 5;
-	vector<int> testSizes = { 10000000 };
+	vector<int> testSizes = { 1000, 10000, 100000, 1000000, 10000000 };
 
 	std::chrono::steady_clock::time_point t1, t2;
 	std::chrono::duration<double> diff;
